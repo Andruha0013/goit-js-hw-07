82525ec74073760d85d21d57923e30f57e33e31c
Andruha0013 <andruha0013@gmail.com>
2022-11-24 18:01:08 +0200

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const divRef=document.querySelector(".gallery");
 console.log(galleryItems);
+
+function cereateGalleryMarkup(items)
+{
+    return items
+    .map(
+        (item) =>`<div class="gallery__item">
+                    <a class="gallery__link" href="${item.original}">
+                    <img
+                        class="gallery__image"
+                        src="${item.preview}"
+                        data-source="${item.original}"
+                        alt="${item.description}"
+                    />
+                    </a>
+                </div>`
+    ).join("");
+}
+
+const addGalleryMarkup=cereateGalleryMarkup(galleryItems);
+divRef.innerHTML=addGalleryMarkup;
+divRef.addEventListener("click",onImgClick);
+
+function onImgClick(evt)
+{
+    blockStandartAction(evt);
+
+    if(evt.target.nodeName!=="IMG")
+    {
+        return;
+    }
+
+    const instance=basicLightbox.create(`<img src="${evt.target.dataset.source}" width="800" height="600">`);
+    instance.show();
+
+    divRef.addEventListener("keydown",(evt)=>{
+        if(evt.code==="Escape")
+        {
+            instance.close();
+        }
+    });
+}
+
+function blockStandartAction(evt)
+{
+    evt.preventDefault();
+}
